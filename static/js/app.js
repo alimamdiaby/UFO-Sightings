4305ec8082929c3aace9f33b31f9b0fc463d0f2a
alimamdiaby <45125526+alimamdiaby@users.noreply.github.com>
2019-03-13 21:34:09 -0400

    Final draft

---
+++ static/js/app.js
@@ -0,0 +1,39 @@
+var tableData  = data;
+var tbody = d3.select("tbody");
+
+tableData.forEach((ufoReport) => {
+   var row = tbody.append("tr");
+   Object.entries(ufoReport).forEach(([key, value]) => {
+       var cell = tbody.append("td");
+       cell.text(value);
+});
+});
+
+function showTable (dataset) {
+    const theData = dataset;
+    var tbody = d3.select("tbody");
+
+    theData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+    });
+});
+};
+
+showTable(data);
+function showValue () {
+   d3.select("tbody").remove();
+   // create a new tbody
+   d3.select("table").append("tbody");
+   var obj = document.getElementById("datetime");
+
+
+    function filteredData(sighting) {
+        return sighting.datetime == obj.value
+    };
+    theData = data.filter(filteredData);
+    console.log(theData)
+    showTable(theData);
+};
